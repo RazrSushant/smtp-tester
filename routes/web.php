d3554c8d9fa02e94f09b1dac2fdc3fa88f831f82
Sushant T <sushant.tayade@razrcorp.com>
2023-08-16 19:42:13 +0530

    random commit

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/', function () {
     // check for failures
     if (Mail::failures()) {
         echo "Failed";
+        $XyZ = "qwe";
         // return response showing failed emails
     }
 });
